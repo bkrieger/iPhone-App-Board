0902bc34e45e04a4f28e3c56ea0393b2e2a41c96
Brandon Krieger <brandontkrieger@gmail.com>
2012-11-05 18:13:11 -0500

    Get posts, can refresh posts, infrastructure for adding posts

---
+++ Board/BTKAddPostViewController.m
@@ -0,0 +1,38 @@
+//
+//  BTKAddPostViewController.m
+//  Board
+//
+//  Created by Brandon Krieger on 11/5/12.
+//  Copyright (c) 2012 Brandon Krieger. All rights reserved.
+//
+
+#import "BTKAddPostViewController.h"
+
+@interface BTKAddPostViewController ()
+
+@end
+
+@implementation BTKAddPostViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
--- Board/BTKMasterViewController.m
+++ Board/BTKMasterViewController.m
@@ -12,6 +12,7 @@
 
 @interface BTKMasterViewController () {
     NSMutableArray *_objects;
+    NSMutableData *_data;
 }
 @end
 
@@ -26,10 +27,20 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-    self.navigationItem.leftBarButtonItem = self.editButtonItem;
+    //self.navigationItem.leftBarButtonItem = self.editButtonItem;
 
-    UIBarButtonItem *addButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(insertNewObject:)];
-    self.navigationItem.rightBarButtonItem = addButton;
+    self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemRefresh target:self action:@selector(getPosts)];
+
+    [self getPosts];
+
+}
+
+- (void)getPosts {
+    //Connect to Message Board
+    NSString *url = @"http://cis195-messages.herokuapp.com/messages";
+    NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:url]];
+    NSURLConnection *connection = [[NSURLConnection alloc] initWithRequest:request delegate:self];
+    [connection start];
 }
 
 - (void)didReceiveMemoryWarning
@@ -38,15 +49,7 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (void)insertNewObject:(id)sender
-{
-    if (!_objects) {
-        _objects = [[NSMutableArray alloc] init];
-    }
-    [_objects insertObject:[NSDate date] atIndex:0];
-    NSIndexPath *indexPath = [NSIndexPath indexPathForRow:0 inSection:0];
-    [self.tableView insertRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
-}
+
 
 #pragma mark - Table View
 
@@ -64,15 +67,16 @@
 {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
 
-    NSDate *object = _objects[indexPath.row];
-    cell.textLabel.text = [object description];
+    NSDictionary *post = _objects[indexPath.row];
+    cell.textLabel.text = [post objectForKey:@"title"];
+    cell.detailTextLabel.text = [post objectForKey:@"body"];
     return cell;
 }
 
 - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
 {
     // Return NO if you do not want the specified item to be editable.
-    return YES;
+    return NO;
 }
 
 - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
@@ -108,6 +112,36 @@
         NSDate *object = _objects[indexPath.row];
         [[segue destinationViewController] setDetailItem:object];
     }
+}
+
+#pragma mark - NSURLConnectionDataDelegate methods
+
+/**
+ * Here are the NSURLConnectionDataDelegate methods that handle the callbacks.
+ * This is mostly primarily and three step process, assuming you get no errors.
+ *
+ * 1. You receive a response.
+ * 2. You receive any number of pieces of data.
+ * 3. The connection finishes loading. That is, you are ready to use the combined pieces of data.
+ */
+
+- (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response {
+    _data = [[NSMutableData alloc] init];
+}
+
+- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data {
+    [_data appendData:data];
+}
+
+- (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error {
+    NSLog(@"Error getting posts");
+}
+
+- (void)connectionDidFinishLoading:(NSURLConnection *)connection {
+    NSDictionary *posts = [NSJSONSerialization JSONObjectWithData:_data options:0 error:nil];
+    NSLog(@"%@", posts); // If you want to see what the posts response looks like.
+    _objects = (NSMutableArray*)posts;
+    [self.tableView reloadData];
 }
 
 @end
